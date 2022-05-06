31a00dd20ab782a3108e128c3e7e3862271c1d21
ehsherrod <101366207+ehsherrod@users.noreply.github.com>
2022-05-06 14:14:23 -0400

    Add files via upload
    New working square demo code!

--- squaredemo.py
+++ squaredemo.py
@@ -5,15 +5,9 @@ import csv
 import numpy as np
 import pandas as pd
 
-# window and pixels. if you make one that isn't the right size, it will tell you in warnings in the terminal
-full_scr = input('full screen? 1 (yes) or 0 (no)')
-if full_scr:
-	# 1440 x 900 is size of my mac
-	win = win = visual.Window([1440,900], color='black', fullscr=0)
-else:
-	win = visual.Window([600,400], color='black', fullscr=0)
+win = win = visual.Window([1440,900], color='black', fullscr=0)
 
-square1 = visual.Rect(win, units='pix', lineWidth=None, pos=(200,200), fillColor=(238,217,56), width=100, height=100, fillColorSpace='rgb')
+square1 = visual.Rect(win, units='pix', pos=(200,200), fillColor=(238,217,56), lineColor=(238,217,56), width=100, height=100, fillColorSpace='rgb255', lineColorSpace='rgb255')
 
 square1.draw()
 win.flip()
