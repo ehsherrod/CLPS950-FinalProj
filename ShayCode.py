933f62e66aec807e256082a697ac0852d2a9a519
Shay Yu <shuah_yu@brown.edu>
2022-05-06 15:50:54 -0400

    Update ShayCode.py

--- ShayCode.py
+++ ShayCode.py
@@ -8,7 +8,7 @@ import pandas as pd
 import random
 
 # creating window
-win = psychopy.visual.Window(size=[800,600], units='pix', color='white', fullscr=0)
+win = visual.Window([1400,900], color='black', fullscr=0)
 mymouse = event.Mouse(visible=True, win=win)
 
 # instructions page
@@ -27,22 +27,22 @@ random.shuffle(shuffled_colors)
 
 tiles = []
 for i in range(len(correct_color_order)):
-    newtile = visual.Rect(win,pos=((-.5+(i*.2)),.5),fillColor=shuffled_colors[i],width=.2,height=.2,fillColorSpace='rgb255')
+    newtile = visual.Rect(win,pos=((-.5+(i*.2)),.5),fillColor=shuffled_colors[i],width=.2,height=.2,fillColorSpace='hex')
     print(newtile.fillColor)
     tiles.append(newtile)
 for t in tiles:
     t.draw()
 
-init_colors = ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF']
+init_colors = ['#000000', '#000000', '#000000', '#000000', '#000000']
 
 puzzlekey = []
 for i in range(len(init_colors)):
-    newkey = visual.Rect(win,pos=((-.5+(i*.2)),0),fillColor=init_colors[i],width=.2,height=.2,fillColorSpace='rgb255')
+    newkey = visual.Rect(win,pos=((-.5+(i*.2)),0),fillColor=init_colors[i],width=.2,height=.2,fillColorSpace='hex')
     puzzlekey.append(newkey)
 for pk in puzzlekey:
     pk.draw()
 
-done_button = visual.Rect(win,pos=(0,-.5),fillColor='black',width=.2,height=.2)
+done_button = visual.Rect(win,pos=(0,-.5),fillColor='white',width=.2,height=.2)
 done_button.draw()
 
 
